6ffaf02cc1f178f5351b45a2be136c3265b8779f
rachelhall <thharbin@gmail.com>
2022-07-08 15:36:03 -0700

    user model testing

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -20,9 +20,10 @@ class Command(BaseCommand):
             try:
                 self.check(databases=['default'])
                 db_up = True
-                except (Psycopg2OpError, OperationalError):
-                    self.stdout.write(
-                        'Database unavailable, waiting 1 second...')
-                    time.sleep(1)
+            except (Psycopg2OpError, OperationalError):
 
-        # self.stdout.write(self.style.SUCCESS('Database available!'))
+                self.stdout.write(
+                    'Database unavailable, waiting 1 second...')
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS('Database available!'))
