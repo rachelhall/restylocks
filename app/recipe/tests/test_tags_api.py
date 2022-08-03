b30e2c702b468e3116a41a600cc24167d4bb72aa
rachelhall <thharbin@gmail.com>
2022-08-03 12:43:28 -0700

    broken test

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -16,6 +16,11 @@ from recipe.serializers import TagSerializer
 TAGS_URL = reverse('recipe:tag-list')
 
 
+def detail_url(tag_id):
+    """Create and return a tag detail url."""
+    return reverse('recipe:tag-detail', args=tag_id)
+
+
 def create_user(email='user@example.com', password="testpass123"):
     """Create and return a user."""
     return get_user_model().objects.create_user(email=email, password=password)
@@ -33,35 +38,59 @@ class PublicTagsApiTests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
-    class PrivateTagsApiTests(TestCase):
-        """Test authenticated API requests."""
 
-        def setUp(self):
-            self.user = create_user()
-            self.client = APIClient()
-            self.client.force_authenticate(self.user)
+class PrivateTagsApiTests(TestCase):
+    """Test authenticated API requests."""
 
-        def test_retrieve_tags(self):
-            """Test retrieving a list of tags."""
-            Tag.object.create(user=self.user, name='Vegan')
-            Tag.objects.create(user=self.user, name='Dessert')
+    def setUp(self):
+        self.user = create_user()
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
 
-            res = self.client.get(TAGS_URL)
+    def test_retrieve_tags(self):
+        """Test retrieving a list of tags."""
+        Tag.object.create(user=self.user, name='Vegan')
+        Tag.objects.create(user=self.user, name='Dessert')
 
-            tags = Tag.objects.all().order_by('-name')
-            serializer = TagSerializer(tags, many=True)
-            self.assertEqual(res.status_code, status.HTTP_200_OK)
-            self.assertEqual(res.data, serializer.data)
+        res = self.client.get(TAGS_URL)
 
-        def test_tags_limited_to_user(self):
-            """Test list of tags is limited to authenticated user."""
-            user2 = create_user(email='user2@example.com')
-            Tag.objects.create(user=user2, name='Fruity')
-            tag = Tag.objects.create(user=self.user, name='Comfort Food')
+        tags = Tag.objects.all().order_by('-name')
+        serializer = TagSerializer(tags, many=True)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
 
-            res = self.client.get(TAGS_URL)
+    def test_tags_limited_to_user(self):
+        """Test list of tags is limited to authenticated user."""
+        user2 = create_user(email='user2@example.com')
+        Tag.objects.create(user=user2, name='Fruity')
+        tag = Tag.objects.create(user=self.user, name='Comfort Food')
 
-            self.assertEqual(res.status_code, status.HTTP_200_OK)
-            self.assertEqual(len(res.data), 1)
-            self.assertEqual(res.data[0]['name', tag.name])
-            self.assertEqual(res.data[0['id'], tag.id])
+        res = self.client.get(TAGS_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data[0]['name', tag.name])
+        self.assertEqual(res.data[0['id'], tag.id])
+
+    def test_update_tag(self):
+        """Test updating a tag."""
+        tag = Tag.objects.create(user=self.user, name='After Dinner')
+
+        payload = {'name': 'Dessert'}
+        url = detail_url(tag.id)
+        res = self.client.patch(url, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        tag.refresh_from_db()
+        self.assertEqual(tag.name, payload['name'])
+
+    def test_delete_tag(self):
+        """Test deleting a tag"""
+        tag = Tag.objects.create(user=self.user, name='Breakfast')
+
+        url = detail_url(tag.id)
+        res = self.client.delete(url)
+
+        self.assertEqual(res.status, status.HTTP_204_NO_CONTENT)
+        tags = Tag.objects.filter(user=self.user)
+        self.assertFalse(tags.exists())
